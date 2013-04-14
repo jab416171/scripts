3e1440f813a5d57c315eee0c9382cd7836c7854b
Joseph Bass <jab416171@gmail.com>
2013-04-13 22:09:40 -0600

    Update install_chrome.sh
    Fixing the chrome script

--- install_chrome.sh
+++ install_chrome.sh
@@ -2,7 +2,7 @@
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
-sudo echo deb http://dl.google.com/linux/chrome/deb/ stable main >> /etc/apt/sources.list.d/google.list
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
 
 sudo apt-get update
 sudo apt-get install google-chrome-stable
